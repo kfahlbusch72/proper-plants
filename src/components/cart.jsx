9856e6493868153d7af752d926fdb36722c4468f
karl <karl.fahlbusch7241@gmail.com>
2025-07-08 11:48:53 -0500

    add clear cart to card jsx page

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,4 +1,4 @@
-function Cart({ cart, addCart, removeCart }) {
+function Cart({ cart, addCart, removeCart, clearCart }) {
   return (
     <div>
       <h2>Cart</h2>
@@ -6,15 +6,22 @@ function Cart({ cart, addCart, removeCart }) {
         {cart.length === 0 ? (
           <p>Nothing in cart.</p>
         ) : (
-          cart.map((item) => (
-            <div key={item.id} className="cart-item">
-              <button onClick={() => removeCart(item)}>-</button>
-              <span>
-                {item.image} {item.name}: {item.quantity}
-              </span>
-              <button onClick={() => addCart(item)}>+</button>
-            </div>
-          ))
+          <>
+            {cart.map((item) => (
+              <div key={item.id} className="cart-item">
+                <button onClick={() => removeCart(item)}>-</button>
+                <span>
+                  {item.image} {item.name}: {item.quantity}
+                </span>
+                <button onClick={() => addCart(item)}>+</button>
+              </div>
+            ))}
+
+            {/* Clear Cart Button */}
+            <button className="clear-cart-btn" onClick={clearCart}>
+              Clear Cart
+            </button>
+          </>
         )}
       </div>
     </div>
